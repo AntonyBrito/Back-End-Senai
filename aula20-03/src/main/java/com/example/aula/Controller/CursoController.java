4f0b66043d112e624843e9f84e0a11d4bec97815
antony <antony.brito047@hotmail.com>
2025-03-26 16:44:40 -0300

    Aula finalizada

--- aula20-03/src/main/java/com/example/aula/Controller/CursoController.java
+++ aula20-03/src/main/java/com/example/aula/Controller/CursoController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/curso")
@@ -28,10 +30,50 @@ public class CursoController {
         }
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Curso> getById(@PathVariable Long id){
+        Optional<Curso> cursoOptional = cursoRepository.findById(id);
+        if(cursoOptional.isPresent()){
+            Curso curso = cursoOptional.get();
+            return ResponseEntity.ok(curso);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
     @PostMapping
     public ResponseEntity<Curso> create(@RequestBody Curso curso){
         Curso cursoBd = cursoRepository.save(curso);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(cursoBd);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(@PathVariable Long id, @RequestBody Curso updateCurso){
+        Optional<Curso> optionalCurso = cursoRepository.findById(id);
+
+        if(optionalCurso.isPresent()){
+            Curso curso = optionalCurso.get();
+            curso.setNome(updateCurso.getNome());
+            curso.setNumeroSala(updateCurso.getNumeroSala());
+            curso.setProfessor(updateCurso.getProfessor());
+
+            return ResponseEntity.ok(cursoRepository.save(curso));
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Curso não encontrado");
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id){
+        Optional<Curso> cursoOptional = cursoRepository.findById(id);
+
+        if(cursoOptional.isPresent()){
+            Curso curso = cursoOptional.get();
+            cursoRepository.delete(curso);
+            return ResponseEntity.ok("Curso deletado com sucesso");
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Curso não encontrado");
+        }
     }
 }
