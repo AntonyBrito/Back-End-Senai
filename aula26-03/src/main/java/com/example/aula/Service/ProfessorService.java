8479177e6983b40244d558c64d903965092aff2f
antony <antony.brito047@hotmail.com>
2025-03-26 17:23:21 -0300

    Aula do dia 26 de março realizada

---
+++ aula26-03/src/main/java/com/example/aula/Service/ProfessorService.java
@@ -0,0 +1,27 @@
+package com.example.aula.Service;
+
+import com.example.aula.DTO.ProfessorDTO;
+import com.example.aula.Entity.Professor;
+import com.example.aula.Repository.ProfessorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfessorService {
+    @Autowired
+    private ProfessorRepository professorRepository;
+
+    public Professor fromDTO(ProfessorDTO professorDTO){
+        Professor professor = new Professor();
+        professor.setNome(professorDTO.getNome());
+        professor.setCpf(professorDTO.getCpf());
+
+        return professor;
+    }
+
+    public Professor save(ProfessorDTO professorDTO){
+        Professor professor = this.fromDTO(professorDTO);
+        Professor professorBd = professorRepository.save(professor);
+        return professorBd;
+    }
+}
